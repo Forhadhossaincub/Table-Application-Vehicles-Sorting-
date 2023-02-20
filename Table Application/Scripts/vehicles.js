2ecefeb345798a4caaec3d76757602c7a840e095
IT Person <IT Person@DESKTOP-MVKFT32>
2023-02-20 15:55:41 +0600

    Add project files.

---
+++ Table Application/Scripts/vehicles.js
@@ -0,0 +1,12 @@
+﻿function init() {
+    document.getElementById('showAntique').addEventListener('click', showAntiqueCars);
+    document.getElementById('showNonAntique').addEventListener('click',showNonAntiqueCars);
+}
+function showAntiqueCars() {
+    document.getElementById('antiqueCars').className = "visible";
+    document.getElementById('nonAntiqueCars').className = "hidden";
+}
+function showNonAntiqueCars() {
+    document.getElementById('antiqueCars').className = "hidden";
+    document.getElementById('nonAntiqueCars').className = "visible";
+}
